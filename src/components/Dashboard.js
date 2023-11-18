ace302ddc8c2fc658913bfeb8ab7fed0b257650e
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-11-18 13:23:53 +0530

    feat: create new department

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,82 @@
-import React from "react";
 import SearchBar from "./Search";
 import trash from "../img/trash-2.svg";
 import edit from "../img/edit-2.svg";
 import DepartmentCard from "./DepartmentCard";
+import { useState, useEffect } from "react";
+import AddPopup from "./AddPopup";
 
 const Dashboard = () => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [departments, setDepartments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleOpenPopup = () => {
+    setIsPopupVisible(true);
+  };
+
+  useEffect(() => {
+    const fetchDepartments = async () => {
+      try {
+        const authToken =
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGZlMTc4MjMyMzIwNmVmMjViOWJiOSIsImlhdCI6MTcwMDI4NDA2MywiZXhwIjoxNzAwNTQzMjYzfQ.rSQ0blzVCB5LfqJDagQW78YUkfsb7Cmt2bf97Fetems";
+
+        const response = await fetch(
+          "https://shlok-mittal-lawyer-backend.vercel.app/api/v1/admin/department",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setDepartments(data.data);
+        } else {
+          setError("Failed to fetch departments");
+        }
+      } catch (error) {
+        setError("Network error. Please try again.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDepartments();
+  }, []);
+
   return (
     <div>
       <div className="p-6 px-12 shadow-md">
         <div className="flex justify-between items-center">
-          <h1 className=" text-3xl font-medium">Deparments</h1>
+          <h1 className=" text-3xl font-medium">Departments</h1>
           <div className="flex justify-center items-center gap-6">
             <SearchBar />
-            <button className="bg-[#0F2C64] text-white py-1 px-2 rounded">
+            <button
+              className="bg-[#0F2C64] text-white py-1 px-2 rounded"
+              onClick={handleOpenPopup}
+            >
               Create Department
             </button>
             <img src={trash} alt="" />
             <img src={edit} alt="" />
           </div>
         </div>
-        <div></div>
       </div>
-      <div className="flex flex-wrap p-10">
-        <DepartmentCard />
+      <div className="flex flex-wrap p-10 gap-10">
+        {loading && <p>Loading...</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {departments.map((department) => (
+          <DepartmentCard
+            key={department._id}
+            departmentName={department.name}
+          />
+        ))}
       </div>
+      {isPopupVisible && <AddPopup setIsPopupVisible={setIsPopupVisible} />}
     </div>
   );
 };
--- src/components/DepartmentCard.js
+++ src/components/DepartmentCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import department from "../img/department.svg"
 
-const DepartmentCard = () => {
+const DepartmentCard = ({departmentName}) => {
   return (
-    <div className='p-5 border border-[#CCCCCC] rounded-md text-center'>
+    <div className='p-5 border border-[#CCCCCC] rounded-md text-center w-40'>
         <img src={department} alt="" className='mx-auto'/>
-        <p className='text-sm font-semibold pt-4'>Department name</p>
+        <p className='text-sm font-semibold pt-4'>{departmentName}</p>
     </div>
   )
 }
